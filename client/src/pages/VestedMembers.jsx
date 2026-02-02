2fb5d312f7577fe41b92eeed7a89e575b986b0ed
pnzenang <49215536+pnzenang@users.noreply.github.com>
2026-02-01 19:40:39 -0500

    Revise Updates component message and links
    Updated the message content for the Updates component.

--- client/src/pages/VestedMembers.jsx
+++ client/src/pages/VestedMembers.jsx
@@ -4,14 +4,14 @@ const Updates = () => {
   return (
     <div>
       <h1 className='text-4xl py-36 '>
-        Sorry about your loss.
+        Welcome to SAGICAM-UPDATES
         <br />
-        Please announce your death by sending an email to one of the following
-        SAGI emails: <span className='font-bold'> <a href="https://whatsapp.com/channel/0029Vb79q1vDp2QA0Iq8M608">https://whatsapp.com/channel/0029Vb79q1vDp2QA0Iq8M608</a> </span>
+        Here you will be able to see all the SAGICAM contribution updates on your phones in real time by following SAGICAM-UPDATES channel.
+         <span className='font-bold'> <a href="https://whatsapp.com/channel/0029Vb79q1vDp2QA0Iq8M608">https://whatsapp.com/channel/0029Vb79q1vDp2QA0Iq8M608</a> </span>
         <br />
         <br />
-        And be reminded that no documentation is needed to announce a
-        death.
+       You can also scan the following QR code
+       
       </h1>
     </div>
   )
